8425447417bf465721a8da5a9d9ce2596a7e4c41
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-04-15 21:46:14 +0900

    210415 문1)if 문을 이용한 1보다 큰 홀수 짝수 값출력하기

--- HD_pss_210415_1 study/HD_pss_210415_1 study/Source.cpp
+++ HD_pss_210415_1 study/HD_pss_210415_1 study/Source.cpp
@@ -1,20 +1,47 @@
 #include<stdio.h>
-void main() {
-	int number = 2;
-	int i = 0;
-	while (i<4) {
-		int j = 0;
-		while (j <=i) {
-			printf("%d", number);
-			number += 2;
-			j ++;
-			printf(" ");
-		} 
-		i ++;
-		printf("\n");	
-	}
-	fgetc(stdin);
+/*
+1. 조건문
+2. try catch exception() {
+
 }
 
+void main() {
+	int number = 33;
+		
+	if (number ==0) {
+		printf("0");
+	}
+	else if (number %2 == 0){
+		printf("짝수");
+	}
+	else  {
+		printf("홀수");
+	}
+
+	fgetc(stdin);
+}
+*/
 
 
+void main() {
+	int i;
+	
+	printf("1보다 큰수를 입력해보세요.");
+	printf("\n");
+	scanf_s("%d", &i);
+	
+	
+	if (i == 0) {
+		printf("Error");
+	}
+	else if (i %2==0){
+		printf("짝수");
+	}
+	else {
+		printf("홀수");
+	}
+
+	printf("\n");
+
+	fgetc(stdin);
+}
